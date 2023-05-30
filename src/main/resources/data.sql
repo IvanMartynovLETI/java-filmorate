d28aae906b9a2ae1795b5288c3d5a5a4d3b0fe15
Ivan Martynov <iamartynov@rambler.ru>
2023-05-29 22:31:25 -0900

    fix: data.sql replace to one created in Idea

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,9 +4,9 @@ INSERT INTO mpa(mpa_id, mpa_name) VALUES(3, 'PG-13');
 INSERT INTO mpa(mpa_id, mpa_name) VALUES(4, 'R');
 INSERT INTO mpa(mpa_id, mpa_name) VALUES(5, 'NC-17');
 
-INSERT INTO genre(genre_id, genre_name) VALUES(1, 'Комедия');
-INSERT INTO genre(genre_id, genre_name) VALUES(2, 'Драма');
-INSERT INTO genre(genre_id, genre_name) VALUES(3, 'Мультфильм');
-INSERT INTO genre(genre_id, genre_name) VALUES(4, 'Триллер');
-INSERT INTO genre(genre_id, genre_name) VALUES(5, 'Документальный');
-INSERT INTO genre(genre_id, genre_name) VALUES(6, 'Боевик');
+INSERT INTO genre(genre_id, genre_name) VALUES(1, 'РљРѕРјРµРґРёСЏ');
+INSERT INTO genre(genre_id, genre_name) VALUES(2, 'Р”СЂР°РјР°');
+INSERT INTO genre(genre_id, genre_name) VALUES(3, 'РњСѓР»СЊС‚С„РёР»СЊРј');
+INSERT INTO genre(genre_id, genre_name) VALUES(4, 'РўСЂРёР»Р»РµСЂ');
+INSERT INTO genre(genre_id, genre_name) VALUES(5, 'Р”РѕРєСѓРјРµРЅС‚Р°Р»СЊРЅС‹Р№');
+INSERT INTO genre(genre_id, genre_name) VALUES(6, 'Р‘РѕРµРІРёРє');
