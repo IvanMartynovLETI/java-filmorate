89a315d1bef609ea6c736e45fdfb0fd68461a073
Ivan Martynov <iamartynov@rambler.ru>
2023-05-29 22:14:42 -0900

    fix: add changes to UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 public interface UserStorage {
     User addUser(User user);
@@ -14,4 +16,12 @@ public interface UserStorage {
     User getUserById(Long id);
 
     Collection<User> findAllUsers();
+
+    User addUserToFriends(Long id, Long friendId);
+
+    User deleteUserFromFriend(Long id, Long friendId);
+
+    Optional<List<User>> getCommonFriends(Long id, Long otherId);
+
+    Collection<User> getFriendsOfUser(Long id);
 }
