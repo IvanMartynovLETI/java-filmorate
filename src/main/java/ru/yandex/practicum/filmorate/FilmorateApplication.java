4d873680ba7e7059643c83c2ca577e43a726c017
Igzario <88246005+Igzario@users.noreply.github.com>
2023-06-21 22:13:45 +0300

    add sorted list - postman all ok

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,5 +1,4 @@
 package ru.yandex.practicum.filmorate;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class FilmorateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
-
 }
