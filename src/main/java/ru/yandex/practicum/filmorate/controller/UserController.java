fe59932084443024e27769a5b6363283d55b8e64
Marilu34 <lulima@yandex.ru>
2023-06-23 12:40:07 +0300

    1

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -15,13 +15,11 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
-
 public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public User create(@Valid
-                       @RequestBody User user) {
+    public User create(@Valid @RequestBody User user) {
         log.info("Request for user adding obtained.");
         return userService.addUser(user);
     }
@@ -29,6 +27,7 @@ public class UserController {
     @PutMapping
     public User put(@RequestBody User user) {
         log.info("Request for user modification obtained.");
+
         return userService.modifyUser(user);
     }
 
@@ -40,49 +39,51 @@ public class UserController {
 
     @GetMapping("/{id}")
     @ResponseBody
-    public User getUserById(@Valid
-                            @PathVariable (required = false) final Long id) {
+    public User getUserById(@Valid @PathVariable(required = false) final Long id) {
         log.info("Request for getting user by id obtained.");
+
         return userService.getUserById(id);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
     @ResponseBody
-    public User addUserToFriend(@PathVariable (required = false) final Long id,
-                                @PathVariable (required = false) final Long friendId) {
+    public User addUserToFriend(@PathVariable(required = false) final Long id,
+                                @PathVariable(required = false) final Long friendId) {
         log.info("Request for adding user to friends obtained.");
+
         return userService.addUserToFriends(id, friendId);
     }
 
 
     @DeleteMapping("/{id}/friends/{friendId}")
     @ResponseBody
-    public User deleteUserFromFriends(@PathVariable (required = false) final Long id,
-                                      @PathVariable (required = false) final Long friendId) {
+    public User deleteUserFromFriends(@PathVariable(required = false) final Long id,
+                                      @PathVariable(required = false) final Long friendId) {
         log.info("Request for deleting user from friends obtained.");
+
         return userService.deleteUserFromFriend(id, friendId);
     }
 
     @GetMapping("/{id}/friends")
     @ResponseBody
-    public Collection<User> getFriendsOfUser(@PathVariable (required = false) final Long id) {
+    public Collection<User> getFriendsOfUser(@PathVariable(required = false) final Long id) {
         log.info("Request for getting friends of user obtained.");
+
         return userService.getFriendsOfUser(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     @ResponseBody
-    public Optional<List<User>> getCommonFriends(@PathVariable (required = false) final Long id,
-                                                 @PathVariable (required = false) final Long otherId) {
+    public Optional<List<User>> getCommonFriends(@PathVariable(required = false) final Long id,
+                                                 @PathVariable(required = false) final Long otherId) {
         log.info("Request for getting common friends obtained.");
+
         return userService.getCommonFriends(id, otherId);
     }
-
-    @DeleteMapping
+    @DeleteMapping("{userId}")
     @ResponseBody
-    public void deleteUser(@PathVariable long userId) {
+    public void deleteUser(@PathVariable("userId") long userId) {
         log.info("Пользователь " + userId + " был удален");
         userService.deleteUser(userService.getUserById(userId));
     }
-
 }
