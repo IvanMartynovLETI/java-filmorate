e569eee7ffbd208b0ebb5feacd2a19c4e7284511
Marilu34 <lulima@yandex.ru>
2023-06-23 13:33:47 +0300

    1

--- src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionApiHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionApiHandler.java
@@ -16,13 +16,16 @@ import ru.yandex.practicum.filmorate.exception.*;
 public class ExceptionApiHandler {
     @ExceptionHandler (EntityNotFoundException.class)
     public ResponseEntity <ErrorMessage> entityNotFoundException(EntityNotFoundException e) {
+
         log.warn(e.toString());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorMessage(e.getMessage()));
 
     }
 
     @ExceptionHandler (MethodArgumentNotValidException.class)
     public ResponseEntity <ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+
         log.warn(e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessage(e.getMessage()));
 
@@ -30,7 +33,8 @@ public class ExceptionApiHandler {
 
     @ExceptionHandler (IncorrectParameterException.class)
     public ResponseEntity <ErrorMessage> incorrectParameterException(IncorrectParameterException e) {
-        log.warn(e.toString());
+
+       log.warn(e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).
                 body(new ErrorMessage(e.getMessage()));
 
@@ -39,10 +43,12 @@ public class ExceptionApiHandler {
     @ExceptionHandler
     @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleRunTimeException(final RuntimeException e) {
+
         log.warn(e.getMessage());
         return new ErrorMessage(
                 e.getMessage()
         );
+
     }
 
 }
