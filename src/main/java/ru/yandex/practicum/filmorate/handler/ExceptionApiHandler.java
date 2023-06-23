2608d9f73f9f3d1ca1d571160f4801319ee1bdb0
Marilu34 <lulima@yandex.ru>
2023-06-23 13:30:03 +0300

    1

--- src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionApiHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ExceptionApiHandler.java
@@ -28,7 +28,7 @@ public class ExceptionApiHandler {
     }
 
     @ExceptionHandler (IncorrectParameterException.class)
-    public ResponseEntity<ErrorMessage> incorrectParameterException(IncorrectParameterException e) {
+    public ResponseEntity <ErrorMessage> incorrectParameterException(IncorrectParameterException e) {
         log.warn(e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).
                 body(new ErrorMessage(e.getMessage()));
